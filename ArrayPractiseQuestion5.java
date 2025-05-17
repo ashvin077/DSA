55b3058a6edc39c9ed1aa1d56927da65e53eb36b
ashvin077 <iam.ashvindhakal@gmail.com>
2025-05-17 20:47:56 +0545

    Reversing the array

--- ArrayPractiseQuestion5.java
+++ ArrayPractiseQuestion5.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class ArrayPractiseQuestion5 {
+// Reverse the given array
+
+public class ArrayPractiseQuestion5 {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5,6};
+        int l = arr.length;
+        int n = Math.floorDiv(l, 2);
+        int temp;
+
+        for (int i = 0; i < n; i++){
+            // swap a[i] and a[l-1-i]
+            temp = arr[i];
+            arr[i] = arr[l-1-i];
+            arr[l-1-i] = temp;
+        }
+        for (int element : arr){
+            System.out.print(element+" ");
+        }
+    }
 }
