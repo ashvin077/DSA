143199acc7c768510b5166209064fc027a6f202c
ashvin077 <iam.ashvindhakal@gmail.com>
2025-05-17 20:44:50 +0545

    Finding whether a integer is present in array or not

--- ArrayPractiseQuestion2.java
+++ ArrayPractiseQuestion2.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class ArrayPractiseQuestion2 {
+// Write a program to find out whether a given integer is present in an array or not.
+
+import java.util.Scanner;
+
+public class ArrayPractiseQuestion2 {
+    public static void main(String[] args) {
+        Scanner number = new Scanner(System.in);
+        System.out.print("Enter number to check: ");
+        float num_check = number.nextFloat();
+        float [] marks = {20.5f,30.4f,25.6f,28.5f,24.3f};
+        boolean isInArray = false;
+
+        for (float elements : marks){
+            if(num_check == elements) {
+                isInArray = true;
+                break;
+            }
+        }
+        if(isInArray){
+            System.out.println("The number is in Array");
+        }
+        else {
+            System.out.println("The number is in not in Array");
+        }
+    }
 }
