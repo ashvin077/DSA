e64ba5c666fe031907b5c3849ff52002f0b1e57f
ashvin077 <iam.ashvindhakal@gmail.com>
2025-05-20 20:47:57 +0545

    Stack

--- Stack.java
+++ Stack.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Stack {
+public class Stack {
+    public static void main(String[] args) {
+
+        //  A Stack is a linear data structure that follows the Last In, First Out (LIFO) principle.
+        //  This means the last element added to the stack is the first one to be removed.
+        // Operations:
+            //  push(item) – Adds an item to the top of the stack.
+            //  pop() – Removes the item from the top of the stack.
+            //  peek()/top() – Returns the item at the top without removing it.
+            //  isEmpty() – Checks whether the stack is empty.
+        // Time Complexity : O(1)
+
+
+
+
+
+    }
 }
